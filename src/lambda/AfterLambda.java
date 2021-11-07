b6aff574f28be666648d442c03ca7c878445d6f4
Tany <juni8453@naver.com>
2021-11-07 20:39:10 +0900

    람다 간단 정리

---
+++ src/lambda/AfterLambda.java
@@ -0,0 +1,35 @@
+package lambda;
+
+@FunctionalInterface
+interface Good {
+    void doSome();
+}
+
+// public class AfterLambda implements Good {
+//     @Override
+//     public void doSome() {
+//         System.out.println("do Operation !");
+//     }
+// }
+// class Main2 {
+//     public static void main(String[] args) {
+//         Good a = new AfterLambda();
+//         a.doSome();
+//     }
+// }
+
+class Main2 {
+    public static void main(String[] args) {
+        Good a = () -> System.out.println("do Operation !");
+        a.doSome();
+    }
+}
+
+/* 람다식 도입 이전
+* 인터페이스를 생성하고 그 인터페이스를 구현하는 클래스를 만들어준다.
+* 이후 인터페이스의 메소드를 오버라이드하고, (인터페이스) = new (구현 객체)를 만들어서 메소드를 이용한다.
+*/
+
+/* 람다식 도입 이후
+* 인터페이스를 생성하고 (인터페이스) = () -> ~ 와 같은 람다식을 사용해서 바로 메소드를 이용할 수 있게 되었다.
+* */
