b84670cc14290adaae71b0f5cfe68736faaebf82
Tany <juni8453@naver.com>
2021-11-09 00:09:24 +0900

    2주 1일차 커밋

---
+++ src/codesquad/day6/accountbook/Member.java
@@ -0,0 +1,22 @@
+package codesquad.day6.accountbook;
+
+public class Member {
+    private String name;
+    private String pasword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+}
